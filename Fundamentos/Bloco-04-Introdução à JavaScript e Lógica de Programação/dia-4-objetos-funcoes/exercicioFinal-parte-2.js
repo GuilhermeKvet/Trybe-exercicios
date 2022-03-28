37753786c8cc6127ebd3b6575530a7626edcaf51
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-03-28 20:24:34 -0300

    .

---
+++ Fundamentos/Bloco-04-Introdução à JavaScript e Lógica de Programação/dia-4-objetos-funcoes/exercicioFinal-parte-2.js
@@ -0,0 +1,1 @@
+//https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-objetos-e-funcoes/5901c768-9dee-46e8-b23f-c03e76c5b8b3/exercicios/209a4db4-3ead-4587-8f19-64118b3bec45/parte-ii-funcoes/de3b3550-6789-4790-8647-41c4f97c1fc6?use_case=side_bar
