dc731e63b6a18dbb7c97ebc784cbc5cfe905c0fe
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-05-10 21:36:10 -0300

    Alterando estrutura

---
+++ Front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+import Pokedex from './Pokedex';
+import './App.css';
+import pokemons from './data'; 
+
+function App() {
+  return (
+    <div className = 'App'>
+      <h1> Pokedex </h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+}
+
+export default App;
