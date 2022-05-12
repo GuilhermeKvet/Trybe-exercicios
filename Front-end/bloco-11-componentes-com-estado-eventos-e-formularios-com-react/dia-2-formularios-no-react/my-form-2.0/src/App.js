59f1eba6285c40f0736e04bea8fcf4f3c32f86d3
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-05-12 15:58:36 -0300

    Realizando parte do exercicio do dia 11.2

--- Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import estados from './data';
 
 class App extends React.Component {
   state = {
@@ -14,11 +15,20 @@ class App extends React.Component {
 
   validateAdress = (address) => address.replace(/[^\w\s]/gi, '');
 
+  handlerBlur = (event) => {
+    let { name, value } = event.target;
+    if (name === 'city') value = value.match(/^\d/) ? '' : value;
+    this.updateState(name, value);
+  }
+
   handlerChange = (event) => {
     let { name, value } = event.target;
     if (name === 'name') value = value.toUpperCase();
     if (name === 'address') value = this.validateAdress(value);
-    if (name === 'city') value = value.match(/^\d/) ? '' : value;
+    this.updateState(name, value);
+  }
+
+  updateState = (name, value) => {
     this.setState({
       [name]: value,
     });
@@ -27,39 +37,44 @@ class App extends React.Component {
   render() {
     const { name, email, cpf, address, city, state, type } = this.state;
     return (
-      <form>
-        <label htmlFor='inputName'>Nome:
-          <input type='text' id='inputName' name='name' value={name} onChange={this.handlerChange} maxLength='40' required />
-        </label>
-        <br />
-        <label htmlFor='inputEmail'>Email:
-          <input type='email' id='inputEmail' name='email' value={email} onChange={this.handlerChange} maxLength='50' required />
-        </label>
-        <br />
-        <label htmlFor='cpfInput'>CPF:
-          <input type='text' id='cpfInput' name='cpf' value={cpf} onChange={this.handlerChange} maxLength='11' required />
-        </label>
-        <br />
-        <label htmlFor='inputAddress'>Address:
-          <input type="text" id='inputAddress' name='address' value={address} onChange={this.handlerChange} maxLength='200' required />
-        </label>
-        <br />
-        <label htmlFor='inputCity'>City:
-          <input type="text" id='inputCity' name='city' value={city} onBlur={this.handlerChange} required />
-        </label>
-        <br />
-        <label>State:
-          <select name='state' value={state} required>
-            <option value="" key="">Santa Catarina</option>
-            <option value="" key="">Paraná</option>
-            <option value="" key="">Rio Grande do Sul</option>
-          </select>
-        </label>
-        <br/>
-        <label htmlFor='inputRadio'>Tipo:
-          <input type="radio" id='inputRadio' name='radio' value={type} onChange={this.handlerChange} required />
-        </label>
-      </form>
+      <fieldset>
+        <form>
+          <label htmlFor='inputName'>Nome:
+            <input type='text' id='inputName' name='name' value={name} onChange={this.handlerChange} maxLength='40' required />
+          </label>
+          <br />
+          <label htmlFor='inputEmail'>Email:
+            <input type='email' id='inputEmail' name='email' value={email} onChange={this.handlerChange} maxLength='50' required />
+          </label>
+          <br />
+          <label htmlFor='cpfInput'>CPF:
+            <input type='text' id='cpfInput' name='cpf' value={cpf} onChange={this.handlerChange} maxLength='11' required />
+          </label>
+          <br />
+          <label htmlFor='inputAddress'>Address:
+            <input type="text" id='inputAddress' name='address' value={address} onChange={this.handlerChange} maxLength='200' required />
+          </label>
+          <br />
+          <label htmlFor='inputCity'>City:
+            <input type="text" id='inputCity' name='city' value={city} onBlur={this.handlerBlur} onChange={this.handlerChange} maxLength='28' required />
+          </label>
+          <br />
+          <label>State:
+            <select name='state' value={state} onChange={this.handlerChange} required>
+              {estados.map((estado) =>
+                (<option key={estado.nome}>{estado.nome}</option>)
+              )}
+            </select>
+          </label>
+          <br />
+          <label htmlFor='house'>Casa:
+            <input type="radio" id='inputRadio' name='radio' value={type} onChange={this.handlerChange} required />
+          </label>
+          <label htmlFor='apart'>Apartament:
+            <input type="radio" id='apart' name='radio' value={type} onChange={this.handlerChange} required />
+          </label>
+        </form>
+      </fieldset>
     );
   }
 }
