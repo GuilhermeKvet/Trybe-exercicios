347891095f511357e72d522a83d4e74df9deb254
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-06-20 20:52:16 -0300

    Realizando exercicio dia 15.4

---
+++ Front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/src/TrafficSignal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { changeSignalAction } from './redux/actions';
+import redSignal from './images/redSignal.jpeg';
+import greenSignal from './images/greenSignal.jpeg';
+import yellowSignal from './images/yellowSignal.jpeg';
+
+const renderSignal = (signalColor) => {
+  switch (signalColor) {
+  case 'red':
+    return redSignal;
+  case 'green':
+    return greenSignal;
+  case 'yellow':
+    return yellowSignal;
+  default:
+    return null;
+  }
+};
+
+function TrafficSignal({ signalColor, changeSignal }) {
+  return (
+    <div>
+      <div className="button-container">
+        <button type="button" onClick={ () => changeSignal('red') }>Red</button>
+        <button type="button" onClick={ () => changeSignal('yellow') }>Yellow</button>
+        <button type="button" onClick={ () => changeSignal('green') }>Green</button>
+      </div>
+      <img className="signal" src={ renderSignal(signalColor) } alt="" />
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  signalColor: state.trafficReducer.signal.color,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSignal: (color) => dispatch(changeSignalAction(color)),
+});
+
+TrafficSignal.propTypes = {
+  signalColor: PropTypes.string.isRequired,
+  changeSignal: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
