2d6e6ad39d918f1700fd068aa68bfd7bd62ad3e0
Guilherme Vinicius Kvet da Rocha <guilhermevkvet22@gmail.com>
2022-08-23 23:07:52 -0300

    Revert "."
    This reverts commit 943ff006fce1a5e1ee48343d3efa8ebe675ca1a4.

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercise1/my-scripts.js
+++
@@ -1,9 +0,0 @@
-const readLine = require('readline-sync');
-const { bmiCalc } = require('./bmi');
-
-const weigth = readLine.questionFloat("What' your weight?");
-const height = readLine.questionFloat("What' your height?");
-
-const bmiResult = bmiCalc(weigth, height);
-
-console.log(bmiResult);
--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/src/server.js
+++
@@ -1,3 +0,0 @@
-const app = require('./app');
-
-app.listen(3002, () => console.log('Servidor online na porta 3002'));
