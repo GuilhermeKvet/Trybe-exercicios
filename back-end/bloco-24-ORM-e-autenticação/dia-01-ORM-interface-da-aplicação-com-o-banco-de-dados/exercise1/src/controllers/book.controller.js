6cd6d76acd7a14fc80d0f4cd5147481ee07bd59c
Guilherme Vinicius Kvet da Rocha <guilhermevkvet22@gmail.com>
2022-09-16 20:28:41 -0300

    Completando exercicios do dia 24.1

--- back-end/bloco-24-ORM-e-autenticação/dia-01-ORM-interface-da-aplicação-com-o-banco-de-dados/exercise1/src/controllers/book.controller.js
+++ back-end/bloco-24-ORM-e-autenticação/dia-01-ORM-interface-da-aplicação-com-o-banco-de-dados/exercise1/src/controllers/book.controller.js
@@ -1,5 +1,7 @@
 const BookService = require('../services/book.service');
 
+const error500Message = 'An error has occurred';
+
 const getAll = async (req, res) => {
   try {
     const books = await BookService.getAll();
@@ -7,10 +9,67 @@ const getAll = async (req, res) => {
     return res.status(200).json(books);
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Ocorreu um erro' });
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+  
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const creteBook = async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const newBook = await BookService.createUser(title, author, pageQuantity);
+
+    return res.status(201).json(newBook);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const { id } = req.params;
+    const updateBook = await BookService.updateUser(id, title, author, pageQuantity);
+
+    if (!updateBook) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json({ message: 'Book successfully updated!' });    
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await BookService.deleteBook(id);
+
+    return res.status(200).json({ message: 'Book successfully deleted!' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
   }
 };
 
 module.exports = {
   getAll,
+  getById,
+  creteBook,
+  updateBook,
+  deleteBook,
 };
--- back-end/bloco-24-ORM-e-autenticação/dia-01-ORM-interface-da-aplicação-com-o-banco-de-dados/exercise1/src/services/book.service.js
+++ back-end/bloco-24-ORM-e-autenticação/dia-01-ORM-interface-da-aplicação-com-o-banco-de-dados/exercise1/src/services/book.service.js
@@ -5,6 +5,37 @@ const getAll = async () => {
   return books;
 };
 
+const getById = async (id) => {
+  const book = await Book.findByPk(id);
+  return book;
+}
+
+const createBook = async (title, author, pageQuantity) => {
+  const newBook = await Book.create({ title, author, pageQuantity });
+  return newBook;
+};
+
+const updateBook = async (id, title, author, pageQuantity) => {
+  const [updateBook] = await Book.update(
+    { title, author, pageQuantity },
+    { where: { id } },
+  );
+
+  return updateBook;
+};
+
+const deleteBook = async (id) => {
+  const book = await Book.destroy(
+    { where: { id } },
+  );
+
+  return book;
+};
+
 module.exports = {
   getAll,
+  getById,
+  createBook,
+  updateBook,
+  deleteBook
 };
