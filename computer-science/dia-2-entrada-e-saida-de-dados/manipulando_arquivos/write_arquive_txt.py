078f902f991464501a7ad0ed850d56e727dec477
GuilhermeKvet <guilhermevkvet22@gmail.com>
2023-01-20 22:05:26 -0300

    estudando conteudo dia

---
+++ computer-science/dia-2-entrada-e-saida-de-dados/manipulando_arquivos/write_arquive_txt.py
@@ -0,0 +1,11 @@
+# escrita
+file = open("arquivo.txt", mode="w")
+LINES = ["Olá\n", "mundo\n", "belo\n", "do\n", "Python\n"]
+file.writelines(LINES)
+file.close()
+
+# leitura
+file = open("arquivo.txt", mode="r")
+for line in file:
+    print(line)
+file.close()
